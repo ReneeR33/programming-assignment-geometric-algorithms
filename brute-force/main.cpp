4745386fbb40f3e1e8abee78fc35532f57795a00
Renée Rulkens <42935816+ReneeR33@users.noreply.github.com>
2024-01-23 17:06:52 +0100

    added and modified brute force

---
+++ brute-force/main.cpp
@@ -0,0 +1,137 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <exception>
+#include <array>
+
+
+struct Point {
+    Point(uint x, uint y) : x(x), y(y) {}
+    Point() : x(0), y(0) {}
+
+    uint x;
+    uint y;
+};
+
+struct Segment {
+    Segment(Point start, Point end) : start(start), end(end) {}
+    Segment() {}
+
+    Point start;
+    Point end;
+};
+
+void parse(
+    std::istream &input_stream, 
+    std::vector<Segment> &hs_out, 
+    std::vector<Segment> &vs_out 
+) {
+    enum State {
+        R_HORIZONTAL,
+        R_VERTICAL,
+        NONE
+    };
+
+    hs_out.clear();
+    vs_out.clear();
+
+    std::array<size_t, 2> space_indices;
+    std::array<std::string, 3> line_substrings;
+    uint lines_remaining;
+
+    State state = NONE;
+    bool read_horizontal = false;
+    bool read_vertical = false;
+    std::string line;
+
+    while((!read_horizontal || !read_vertical)) {
+        std::getline(input_stream, line);
+
+        if (line.empty()) {
+            throw std::runtime_error("InputParser: Invalid input");
+        }
+
+        space_indices[0] = line.find(' ');
+        space_indices[1] = line.find(' ', space_indices[0] + 1);
+
+        line_substrings[0] = line.substr(0, space_indices[0]);
+        line_substrings[1] = line.substr(space_indices[0] + 1, space_indices[1] - space_indices[0] - 1);
+        line_substrings[2] = line.substr(space_indices[1] + 1, line.size() - space_indices[1] - 1);
+
+        if (line[0] == 'h') {
+            lines_remaining = std::stoul(line_substrings[2]);
+            hs_out.resize(lines_remaining);
+            state = R_HORIZONTAL;
+        } else if (line[0] == 'v') {
+            lines_remaining = std::stoul(line_substrings[2]);
+            vs_out.resize(lines_remaining);
+            state = R_VERTICAL;
+        } else {
+            int index = lines_remaining - 1;
+
+            switch (state) {
+            case R_HORIZONTAL:
+                {
+                    uint x_start = std::stoul(line_substrings[0]);
+                    uint x_end = std::stoul(line_substrings[1]);
+                    uint y_pos = std::stoul(line_substrings[2]);
+
+                    hs_out[index] = Segment(
+                        Point(x_start, y_pos),
+                        Point(x_end, y_pos)
+                    );
+
+                    lines_remaining--;
+                    if (lines_remaining <= 0) {
+                        read_horizontal = true;
+                    }
+                }
+                break;
+
+            case R_VERTICAL:
+                {
+                    uint x_pos = std::stoul(line_substrings[0]);
+                    uint y_start = std::stoul(line_substrings[1]);
+                    uint y_end = std::stoul(line_substrings[2]);
+
+                    vs_out[index] = Segment(
+                        Point(x_pos, y_start),
+                        Point(x_pos, y_end)
+                    );
+
+                    lines_remaining--;
+                    if (lines_remaining <= 0) {
+                        read_vertical = true;
+                    }
+                }
+                break;
+            
+            default:
+                throw std::runtime_error("InputParser: Invalid input");
+                break;
+            }
+        }
+    }
+}
+
+bool intsersects(Segment h, Segment v) {
+    return (v.start.y <= h.start.y && h.start.y <= v.end.y) && (h.start.x <= v.start.x && v.start.x <= h.end.x);
+}
+
+int main() {
+    std::vector<Segment> hs;
+    std::vector<Segment> vs;
+
+    parse(std::cin, hs, vs);
+
+    int intersections = 0;
+    for (auto& h : hs) {
+        for (auto& v : vs) {
+            if (intsersects(h, v)) {
+                intersections++;
+            }
+        }
+    }
+
+    std::cout << intersections << std::endl;
+}
